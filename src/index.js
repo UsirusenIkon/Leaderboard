2175d0d19f7c15012ace510eb6c03a8fa44cb8d0
Usirusen Ikon <lexicon048@gmail.com>
2022-10-13 17:28:47 +0100

    Import renderData.js and fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
 import './style.css';
 import getData from './modules/getData.js';
-import postData from './modules/postData';
+import postData from './modules/postData.js';
+import renderData from './modules/renderData.js';
 
-//sending data to an api
+// sending data to an api
 const form = document.querySelector('.form');
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault();
   const name = document.querySelector('.user');
   const point = document.querySelector('.point');
   const item = {
     user: name.value,
     score: point.value,
-  }
+  };
   postData('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2Ikon/scores/', item);
-})
-
+});
 
 const refreshBtn = document.querySelector('.refresh');
 refreshBtn.addEventListener('click', () => {
   const scores = document.querySelector('.scores');
-  scores.innerHTML = ''
+  scores.innerHTML = '';
   getData()
-  .then(response => response.json())
-  .then(data => {
-  for(let item of data.result) {
-    scores.innerHTML += `<li class="item">${item.user}:${item.score}</li>`
-    console.log(item);
-  }})
-})
+    .then((response) => response.json())
+    .then((data) => {renderData(data.result)});
+});
