ba4ec357f6763ece5c074cef6ab866ce396dd8ba
Usirusen Ikon <lexicon048@gmail.com>
2022-10-13 17:35:50 +0100

    Fix linter errors

--- src/modules/postData.js
+++ src/modules/postData.js
@@ -1,12 +1,12 @@
-const postData = async(url, item) => {
-  const response = await fetch(url,{
-  method: 'POST',
-  body: JSON.stringify(item),
+const postData = async (url, item) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(item),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  })
+  });
   return response;
-}
+};
 
 export default postData;
